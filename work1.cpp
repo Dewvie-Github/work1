eeeac08c3a6bf79838186b8fc484857e6a46d122
Saharat Thongin <was.dew4546@gmail.com>
2020-07-18 19:36:07 +0700

    initial commit

--- work1.cpp
+++ work1.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include<iostream>			//This is for testing
 using namespace std;
 void main() {
 	cout << "  1st term of 2020		\\45132114   Wanthanee Prachuabsupakij" << endl;
 	cout << "--------------------------------------------------------------" << endl;
 	cout << "| Subject ID | Subject Name				| Score(100) | Grade |" << endl; 
 	cout << "--------------------------------------------------------------" << endl;
-	cout << "|   4570034  | Introduction to combuter			|     85     |  \"A\"" << endl; // use \ after " to put " in cout command 
+	cout << "|   4570034  | Introduction to combuter			|     85     |  \"A\"" << endl; // use \ after " to put " in cout command.
 	cout << "|   4580247  | English					|     74     |  \"B\"" << endl;
 	cout << "|   4570034  | Design Thinking				|     92     |  \"A\"" << endl;
 	cout << "--------------------------------------------------------------" << endl;
